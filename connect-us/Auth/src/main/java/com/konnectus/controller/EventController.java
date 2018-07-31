0d43f9813e8beb8fc27a8a9e16cffd614efa4f59
ritesh <ritesh.nailwal@rsystems.com>
2018-07-31 21:28:18 +0530

    associte to user

--- connect-us/Auth/src/main/java/com/konnectus/controller/EventController.java
+++ connect-us/Auth/src/main/java/com/konnectus/controller/EventController.java
@@ -3,12 +3,14 @@ package com.konnectus.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.konnectus.domain.Event;
+import com.konnectus.domain.User;
 import com.konnectus.service.EventService;
 
 @RestController
@@ -25,4 +27,11 @@ public class EventController {
 	public List<Event> getEvents() {
 		return eventService.getEvents();
 	}
+	
+	@RequestMapping(value = "/event/{eventid}/user", method= RequestMethod.POST)
+	public Event addUserToEvent(@PathVariable("eventid") String eventid, @RequestBody User user) {
+		return eventService.addUserToEvent(eventid, user);
+	}
+	
+	
 }
--- connect-us/Auth/src/main/java/com/konnectus/service/EventService.java
+++ connect-us/Auth/src/main/java/com/konnectus/service/EventService.java
@@ -1,13 +1,19 @@
 package com.konnectus.service;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.konnectus.domain.Event;
+import com.konnectus.domain.User;
 import com.konnectus.repository.EventRepository;
 
 
@@ -21,6 +27,20 @@ public class EventService {
 	public Event addEvent(Event event) {
 		_log.info("creating new event");
 		return eventRepository.save(event);
+	}
+	
+	public Event addUserToEvent(String eventid, User user) {
+		Optional<Event> eventOptionalToSave = eventRepository.findById(eventid);
+		if(eventOptionalToSave.isPresent()) {
+			Event eventToSave = eventOptionalToSave.get();
+			Set<User> users = eventToSave.getUsers();
+			if(users == null) {
+				users = new HashSet<>();
+			}
+			users.add(user);
+			return eventRepository.save(eventToSave);
+		}
+		return null;
 	}
 	
 	public List<Event> getEvents() {
